0c71161c6035e6b5bab7fe37c780994cd7f8e1c2
eden-fenster <eden.fenster@gmail.com>
2023-11-05 13:52:18 +0200

    starting word count

---
+++ word_count/word_count.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define IN 1 /* inside */
+#define OUT 0 /* outside */
+
+/* count lines, words and chars */
+main()
+{
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF)
+    {
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT)
+        {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+}
